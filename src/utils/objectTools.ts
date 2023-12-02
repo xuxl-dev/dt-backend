b6b37060bbd353336f6b3f64fead9cc7c2c734b1
bkmashiro <dylan_233@foxmail.com>
2023-12-02 18:31:03 +0800

    Add lodash library

--- src/utils/objectTools.ts
+++ src/utils/objectTools.ts
@@ -1,3 +1,5 @@
+import { cloneDeep, defaultsDeep } from 'lodash'
+
 type ObjectWithNullableProps<T> = {
   [K in keyof T]: T[K] | null
 }
@@ -20,20 +22,6 @@ export function isCallable(obj: any): obj is CallableFunction {
 }
 
 export function mergeDefault<T>(obj: Partial<T>, defaults: T): T {
-  if (!obj) return defaults
-
-  // recursively merge objects
-  const result = { ...obj }
-  Object.keys(defaults).forEach((key) => {
-    if (typeof defaults[key] !== 'object') return
-
-    if (typeof defaults[key] === 'object') {
-      result[key] = mergeDefault(obj[key], defaults[key])
-      return
-    }
-
-    result[key] = obj[key] ?? defaults[key]
-  })
-
-  return result as T
+  const _obj = cloneDeep(obj)
+  return defaultsDeep(_obj, defaults)
 }
