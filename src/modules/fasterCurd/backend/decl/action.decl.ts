a4a29b7b3076a296b2017c69073b6997dcd7c05c
bkmashiro <dylan_233@foxmail.com>
2023-12-03 19:47:53 +0800

    test2

--- src/modules/fasterCurd/backend/decl/action.decl.ts
+++ src/modules/fasterCurd/backend/decl/action.decl.ts
@@ -1,3 +1,9 @@
+import {
+  ExtendsOnly,
+  Intersection,
+  Prettify,
+  Prettify2,
+} from 'src/utils/type.utils'
 import { ClassType } from 'src/utils/utils'
 
 type ActionName = string
@@ -144,75 +150,110 @@ type TupleToNamedObject<T extends any[]> = {
   [K in T[number] as `#${K}`]: K
 }
 
-type TupleToStorage<T extends any[]> = {
+type TupleToStorage<T> = {
   [K in keyof T as `_${Exclude<K, keyof Array<any>> & string}`]: T[K]
 }
 type test = ObjValueTuple<{ a: 1; b: 22; c: 3 }>
 
-function enumeratedObject<T>(object: T): TupleToStorage<ObjValueTuple<T>> {
-  return Object.keys(object).reduce((acc, key) => {
-    acc[`#${key}`] = key
-    return acc
-  }, {}) as any
-}
+type StorageOfObj<T> = TupleToStorage<ObjValueTuple<T>>
+// type StorageOfN<T extends number> = TupleToStorage<ObjValueTuple<T>>
+type Grow<T, A extends Array<T>> = ((x: T, ...xs: A) => void) extends (
+  ...a: infer X
+) => void
+  ? X
+  : never
+type GrowToSize<T, A extends Array<T>, N extends number> = {
+  0: A
+  1: GrowToSize<T, Grow<T, A>, N>
+}[A['length'] extends N ? 0 : 1]
 
-type StorageOf<T> = TupleToStorage<ObjValueTuple<T>>
-type testStore = StorageOf<{ a: 1; b: 22; c: 3 }>
-const ret = enumeratedObject({
-  a: {
-    myKey: { aaa: 6 },
-  },
-  b: 2,
-  c: 3,
-})
+export type FixedArray<T, N extends number> = GrowToSize<T, [], N>
 
-type CreateTransformOption3<T, K extends StorageOf<{ a }>> = {
-  transformQueryRet?: (
-    result: CreateResult
-  ) => K extends StorageOf<{ a: infer A }> ? A : never
-  transformAfter?: (
-    form: T,
-    queryRet: K extends StorageOf<{ a: infer A }> ? A : never
-  ) => any
-}
+type arr<T extends number> = FixedArray<any, T>
+type StorageOf<T extends number> = TupleToStorage<arr<T>>
+
 type testType<U, V> = U extends V ? true : false
+
+export type LabeledActionOptions2<T, K> = CreateActionOption2<T, K>
 
 export function Action3<
   T extends abstract new (...args: any) => InstanceType<T>,
   K
->(options: LabeledActionOptions<InstanceType<T>, K>) {
+>(options: LabeledActionOptions2<InstanceType<T>, K>) {
   return function classDecorator(target: T) {}
 }
 
-type CreateTransformOption2<T, K1 extends StorageOf<{ a }>> = {
-  transform?: (form: T) => T
-  TransformQueryRetInplace?: (result: CreateResult) => void
-  transformQueryRet?: (result: CreateResult) => K1['_0']
-  transformAfter?: (form: T, queryRet: K1['_0']) => any
+type CreateTransformOption2<T, K> = {
+  transformQueryRet?: (result: CreateResult) => K
+  transformAfter?: (form: T, queryRet: K) => any
 }
 
-export type CreateActionOption2<T, K extends StorageOf<{ a }>> = {
+type CreateTransformOption3<T, K> = {
+  transformUpdateQueryRet?: (result: CreateResult) => K
+  transformUpdateAfter?: (form: T, queryRet: K) => any
+}
+
+export type CreateActionOption2<T, K> = {
   method: 'create'
-} & Partial<CreateTransformOption2<T, K>>
+} & Partial<CreateTransformOption2<T, K>> &
+  Partial<CreateTransformOption3<T, K>>
 
-type OmitActionType<T> = Omit<T, 'action' | 'method'>
+type OptionType<T extends ClassType<T>, K> = Prettify<
+  Partial<ExtendsOnly<K, OmitMethod<CreateActionOption2<InstanceType<T>, K>>>>
+> &
+  Prettify<OmitMethod<CreateActionOption2<InstanceType<T>, K>>>
+
+type OmitMethod<T> = Omit<T, 'method'>
 export function Create3<T extends ClassType<T>, K>(
-  options: OmitActionType<CreateActionOption<InstanceType<T>, K>>
+  options: OptionType<T, K>
+  // options: K
+  // options: OmitMethod<CreateActionOption2<InstanceType<T>, K>>
 ) {
-  return Action3<T, K>({ ...options, method: 'create', action: 'create' })
+  return Action3<T, K>({
+    ...options,
+    method: 'create',
+    action: 'create',
+  })
 }
+
+// type CreateTransformOption3<T> = <K>() => {
+//   transformUpdateQueryRet?: (result: CreateResult) => K
+//   transformUpdateAfter?: (form: T, queryRet: K) => any
+// }
+
+type AB<T> = {
+  a: () => T
+  b: (a:T) => any
+}
+
+export function Extract2<
+  T extends abstract new (...args: any) => InstanceType<T>,
+  K extends tmp
+>(options: Transform<K>) {
+  return function classDecorator(target: T) {}
+}
+
+type tmp = {
+  a: () => 666;
+  b: (b: any) => 2;
+}
+type Transform<T extends tmp> = {
+  a: () => 666;
+  b: (b: ReturnType<T['a']>) => 2;
+}
+type Transformed = Transform<tmp>
 
 @Create3({
-  transformQueryRet: (result) => {
-    return {
-      name: 'hello',
-      id: 114514,
-      6: 666,
-    }
-  },
-  transformAfter: (form, queryRet) => {},
+  transformQueryRet: (result) => '123',
+  transformAfter: (form, queryRet) => 213,
+  // transformUpdateQueryRet: (result) => 321,
+})
+@Extract2({
+  a: () => 666,
+  b: (b) => 2,
 })
 class TU {
+  ids: number
   id: number
   name: string
 }
