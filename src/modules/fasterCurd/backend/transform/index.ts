de6f6c11e11640ca92888b455b4d50910f3e5383
bkmashiro <dylan_233@foxmail.com>
2023-12-06 19:49:38 +0800

    stable type infer

--- src/modules/fasterCurd/backend/transform/index.ts
+++ src/modules/fasterCurd/backend/transform/index.ts
@@ -1,16 +1,18 @@
-export type TransformFunction<T, U> = (data: T) => U;
-
+// export type TransformFunction<T, U> = (data: T) => U;
+export interface TransformFunction<T, R> {
+  (source: T): R;
+}
 class Trans<Target> {
   transform<A>(op1: TransformFunction<Target, A>): (data: Target) => A;
-  // transform<A, B, C>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>): <D>(data: D) => C;
-  // transform<A, B, C, D>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>, op3: TransformFunction<C, D>): <E>(data: E) => D;
-  // transform<A, B, C, D, E>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>, op3: TransformFunction<C, D>, op4: TransformFunction<D, E>): <F>(data: F) => E;
-  // transform<A, B, C, D, E, F>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>, op3: TransformFunction<C, D>, op4: TransformFunction<D, E>, op5: TransformFunction<E, F>): <G>(data: G) => F;
-  // transform<A, B, C, D, E, F, G>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>, op3: TransformFunction<C, D>, op4: TransformFunction<D, E>, op5: TransformFunction<E, F>, op6: TransformFunction<F, G>): <H>(data: H) => G;
-  // transform<A, B, C, D, E, F, G, H>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>, op3: TransformFunction<C, D>, op4: TransformFunction<D, E>, op5: TransformFunction<E, F>, op6: TransformFunction<F, G>, op7: TransformFunction<G, H>): <I>(data: I) => H;
-  // transform<A, B, C, D, E, F, G, H, I>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>, op3: TransformFunction<C, D>, op4: TransformFunction<D, E>, op5: TransformFunction<E, F>, op6: TransformFunction<F, G>, op7: TransformFunction<G, H>, op8: TransformFunction<H, I>): <J>(data: J) => I;
-  // transform<A, B, C, D, E, F, G, H, I, J>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>, op3: TransformFunction<C, D>, op4: TransformFunction<D, E>, op5: TransformFunction<E, F>, op6: TransformFunction<F, G>, op7: TransformFunction<G, H>, op8: TransformFunction<H, I>, op9: TransformFunction<I, J>): <K>(data: K) => J;
-  // transform<A, B, C, D, E, F, G, H, I, J>(op1: TransformFunction<A, B>, op2: TransformFunction<B, C>, op3: TransformFunction<C, D>, op4: TransformFunction<D, E>, op5: TransformFunction<E, F>, op6: TransformFunction<F, G>, op7: TransformFunction<G, H>, op8: TransformFunction<H, I>, op9: TransformFunction<I, J>, ...rest: TransformFunction<any, any>[]): <K>(data: K) => any;
+  transform<A, B>(op1: TransformFunction<Target, A>, op2: TransformFunction<A, B>): (data: Target) => B;
+  transform<A, B, C>(op1: TransformFunction<Target, A>, op2: TransformFunction<A, B>, op3: TransformFunction<B, C>): (data: Target) => C;
+  transform<A, B, C, D>(op1: TransformFunction<Target, A>, op2: TransformFunction<A, B>, op3: TransformFunction<B, C>, op4: TransformFunction<C, D>): (data: Target) => D;
+  transform<A, B, C, D, E>(op1: TransformFunction<Target, A>, op2: TransformFunction<A, B>, op3: TransformFunction<B, C>, op4: TransformFunction<C, D>, op5: TransformFunction<D, E>): (data: Target) => E;
+  transform<A, B, C, D, E, F>(op1: TransformFunction<Target, A>, op2: TransformFunction<A, B>, op3: TransformFunction<B, C>, op4: TransformFunction<C, D>, op5: TransformFunction<D, E>, op6: TransformFunction<E, F>): (data: Target) => F;
+  transform<A, B, C, D, E, F, G>(op1: TransformFunction<Target, A>, op2: TransformFunction<A, B>, op3: TransformFunction<B, C>, op4: TransformFunction<C, D>, op5: TransformFunction<D, E>, op6: TransformFunction<E, F>, op7: TransformFunction<F, G>): (data: Target) => G;
+  transform<A, B, C, D, E, F, G, H>(op1: TransformFunction<Target, A>, op2: TransformFunction<A, B>, op3: TransformFunction<B, C>, op4: TransformFunction<C, D>, op5: TransformFunction<D, E>, op6: TransformFunction<E, F>, op7: TransformFunction<F, G>, op8: TransformFunction<G, H>): (data: Target) => H;
+  transform<A, B, C, D, E, F, G, H>(op1: TransformFunction<Target, A>, op2: TransformFunction<A, B>, op3: TransformFunction<B, C>, op4: TransformFunction<C, D>, op5: TransformFunction<D, E>, op6: TransformFunction<E, F>, op7: TransformFunction<F, G>, op8: TransformFunction<G, H>, ...rest: TransformFunction<any, any>[]): (data: Target) => any;
+
   transform(...ops: TransformFunction<any, any>[]) {
     return <I>(data: I) => ops.reduce((acc, op) => op(acc), data);
   }
@@ -26,4 +28,6 @@ const obj = {
   d: 4,
 }
 const trans = new Trans<typeof obj>()
-trans.transform(pick('a', 'b', 'c'))(obj)
+
+trans.transform(pick(['a', 'b', 'c']), pick(['a', 'b']))(obj).b
+
--- src/modules/fasterCurd/backend/transform/transforms/pick.ts
+++ src/modules/fasterCurd/backend/transform/transforms/pick.ts
@@ -1,26 +1,60 @@
-import { TransformFunction } from ".."
+import { TransformFunction } from '..'
 
-type SafePick<T, K> = {
-  [P in keyof T as P extends K ? P : never]: T[P]
-}
+type SimpleObj = Record<string, any>
 
-function pick<T, K>(...keys: K[]) {
-  return (data: T) => {
-    const result = {} as any
-    for (const key of keys) {
-      result[key] = data[key as any]
-    }
-    return result as SafePick<T, K>
+function SafePicker<T extends SimpleObj, K extends keyof T>(fields: K[]) {
+  return function picker(obj: T): Pick<T, K> {
+    const pickedObj: Partial<Pick<T, K>> = {}
+    fields.forEach((field) => {
+      if (obj.hasOwnProperty(field)) {
+        pickedObj[field] = obj[field]
+      }
+    })
+    return pickedObj as Pick<T, K>
   }
 }
 
-export default pick
-
-const testObj = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4,
+function pick<T, R>(fields: string[]): TransformFunction<T, R> {
+  return function picker(obj: T): R {
+    const pickedObj = {} as R;
+    fields.forEach((field) => {
+      if (obj.hasOwnProperty(field)) {
+        pickedObj[field] = obj[field];
+      }
+    });
+    return pickedObj as { [K in keyof R]: R[K] }
+  };
 }
 
-const test = pick<typeof testObj, any>('a', 'b', 'c')(testObj)
+function pick2<T = any, A extends (keyof T)[] = any>(fields: A): TransformFunction<T, Pick<T, A[number]>> {
+  return function picker(obj: T): Pick<T, A[number]> {
+    const pickedObj = {} as Pick<T, A[number]>;
+    fields.forEach((field) => {
+      if (obj.hasOwnProperty(field)) {
+        pickedObj[field] = obj[field];
+      }
+    });
+    return pickedObj;
+  };
+}
+export default pick2
+
+// 示例用法
+interface ExampleObject {
+  name: string
+  age: number
+  city: string
+}
+
+const examplePicker = SafePicker(['name'])
+
+const exampleObj: ExampleObject = {
+  name: 'John',
+  age: 25,
+  city: 'New York',
+}
+
+const pickedResult = examplePicker(exampleObj)
+
+// pickedResult 的类型为 { name: string, age: number }，保留了原始对象的类型信息
+console.log(pickedResult)
