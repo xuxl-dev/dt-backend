fff84b61c6d7ab898873f2f4aa3d1a87c85cf1cd
bkmashiro <dylan_233@foxmail.com>
2023-12-09 22:22:26 +0800

    remove redudant

--- src/modules/fasterCurd/backend/transform/transforms/array/map.ts
+++ src/modules/fasterCurd/backend/transform/transforms/array/map.ts
@@ -2,9 +2,6 @@ import { createTransform } from '..'
 import { TransformFunction } from '../..'
 
 function map<T, A>(transform: (value: T) => A): TransformFunction<T[], A[]> {
-  // return function mapper(array: T[]): A[] {
-  //   return array.map(transform);
-  // };
   return createTransform((array: T[]) => {
     return array.map(transform)
   })
