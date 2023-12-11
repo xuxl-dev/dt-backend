7e65527f84d9be38b4be8874dbe4ce8c866f37ff
bkmashiro <dylan_233@foxmail.com>
2023-12-11 14:58:11 +0800

    Fix transform error handling

--- src/modules/fasterCurd/backend/transform/transforms/array/map.ts
+++ src/modules/fasterCurd/backend/transform/transforms/array/map.ts
@@ -2,9 +2,12 @@ import { createTransform } from '..'
 import { TransformFunction } from '../..'
 
 function map<T, A>(transform: (value: T) => A): TransformFunction<T[], A[]> {
-  return createTransform((array: T[]) => {
-    return array.map(transform)
-  })
+  return createTransform(
+    (array: T[]) => {
+      return array.map(transform)
+    },
+    { name: 'map' }
+  )
 }
 
 export default map
