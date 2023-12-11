2788101701f6617bf1220f38c6cfa6fdb7555572
bkmashiro <dylan_233@foxmail.com>
2023-12-11 22:27:20 +0800

    clean auth

--- src/modules/auth/local.strategy.ts
+++ src/modules/auth/local.strategy.ts
@@ -1,22 +1,21 @@
-import { compareSync } from 'bcryptjs';
-import { PassportStrategy } from '@nestjs/passport';
-import { IStrategyOptions, Strategy } from 'passport-local';
-import { User } from '../user/entities/user.entity';
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { compareSync } from 'bcryptjs'
+import { PassportStrategy } from '@nestjs/passport'
+import { IStrategyOptions, Strategy } from 'passport-local'
+import { User } from '../user/entities/user.entity'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class LocalStorage extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User)
-    private readonly usersRepository: Repository<User>,
+    private readonly usersRepository: Repository<User>
   ) {
-   
     super({
       usernameField: 'username',
       passwordField: 'password',
-    } as IStrategyOptions);
+    } as IStrategyOptions)
   }
 
   async validate(username: string, password: string) {
@@ -24,16 +23,16 @@ export class LocalStorage extends PassportStrategy(Strategy) {
       .createQueryBuilder('user')
       .addSelect('user.password')
       .where('user.username=:username', { username })
-      .getOne();
+      .getOne()
 
     if (!user) {
-      throw new BadRequestException('user not found');
+      throw new BadRequestException('user not found')
     }
 
     if (!compareSync(password, user.password)) {
-      throw new BadRequestException('invalid password');
+      throw new BadRequestException('invalid password')
     }
 
-    return user;
+    return user
   }
-} 
+}
