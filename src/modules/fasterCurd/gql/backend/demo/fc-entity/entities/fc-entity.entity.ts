d33dfccdc3f779f04272e38a5394b2499e971509
bkmashiro <dylan_233@foxmail.com>
2024-02-20 18:05:48 +0800

    add some type helpers

--- src/modules/fasterCurd/gql/backend/demo/fc-entity/entities/fc-entity.entity.ts
+++ src/modules/fasterCurd/gql/backend/demo/fc-entity/entities/fc-entity.entity.ts
@@ -1,6 +1,9 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ClassType } from 'src/utils/utils';
 
 @ObjectType()
+@Query({
+})
 export class FcEntity {
   id: number;
 
@@ -10,3 +13,46 @@ export class FcEntity {
 /*
   Faster CRUD decorators
 */
+
+export function Query<T extends ClassType<T>, K>(
+  cfg: ActionCfg<InstanceType<T>>
+) {
+  return function classDecorator(target: T) {
+
+  }
+}
+
+type ActionCfg<T extends InstanceType<any>> = {
+  requires?: PartialTuple<KeyTuple<T>>
+}
+
+// union to intersection of functions
+type UnionToIoF<U> =
+    (U extends any ? (k: (x: U) => void) => void : never) extends
+    ((k: infer I) => void) ? I : never
+
+// return last element from Union
+type UnionPop<U> = UnionToIoF<U> extends { (a: infer A): void; } ? A : never;
+
+// prepend an element to a tuple.
+type Prepend<U, T extends any[]> =
+    ((a: U, ...r: T) => void) extends (...r: infer R) => void ? R : never;
+
+type UnionToTupleRecursively<Union, Result extends any[]> = {
+    1: Result;
+    0: UnionToTupleRecursively_<Union, UnionPop<Union>, Result>;
+    // 0: UnionToTupleRecursively<Exclude<Union, UnionPop<Union>>, Prepend<UnionPop<Union>, Result>>
+}[[Union] extends [never] ? 1 : 0];
+
+type UnionToTupleRecursively_<Union, Element, Result extends any[]> =
+    UnionToTupleRecursively<Exclude<Union, Element>, Prepend<Element, Result>>;
+
+type UnionToTuple<U> = UnionToTupleRecursively<U, []>;
+
+type KeyTuple<T> = UnionToTuple<keyof T>
+
+type PartialTuple<T> = {
+  [K in keyof T]?: T[K] | undefined
+}
+
+type FieldSelecor<T> = keyof T
--- src/modules/fasterCurd/gql/fc/fc.service.ts
+++ src/modules/fasterCurd/gql/fc/fc.service.ts
@@ -1,45 +1,11 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as fs from 'fs/promises';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+/*
+  FC GQL is to generate Data dict for faster-curd,
+  resolvers, entity, etc. shall still be defined by user.
+*/
 
 @Injectable()
 export class FcGQLService {
@@ -47,17 +13,14 @@ export class FcGQLService {
     private readonly configService: ConfigService
   ) { }
 
+  collectResolvers() {
+    // collect resolvers from metadata
+    //TODO
+
+    return {}
+  }
+
   async start() {
-    const server = new ApolloServer({
-      // typeDefs,
-      typeDefs: await fs.readFile(this.configService.getOrThrow('SCHEMA_PATH'), 'utf-8'),
-      resolvers,
-    });
-
-    const { url } = await startStandaloneServer(server, {
-      listen: { port: 4000 },
-    });
-
-    console.log(`🚀  Server ready at: ${url}`);
+    
   }
 }
